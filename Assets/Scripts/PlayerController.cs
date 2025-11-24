f328916e7411540817a4b6115bd11d6061761019
justgrindingweb <justgrindinggames@gmail.com>
2025-11-23 19:49:11 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+// Handles player input and actions and stuff
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    ///  These variables will be relative to the environment's scale to support dimension switching.
+    /// </summary>
+    public float speedMultiplier = 5.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
